e404c70fcc39dcdda4fa594040c545541369f8ca
johnrwolfe <j_r_wolfe@msn.com>
2022-02-15 17:04:44 -0700

    Update marking files
    Mark application as a Spring application and mark all ports on
    CarparkControl component as WebSocket ports so that code necessary to
    support WebSocket communication with clients is generated.

--- run-autotest-servlet.sh
+++ run-autotest-servlet.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-CIERA_VERSION=2.3.1-RELEASE
+CIERA_VERSION=2.5.4-SNAPSHOT
 CLASSPATH=$HOME/.m2/repository/io/ciera/runtime/$CIERA_VERSION/runtime-$CIERA_VERSION.jar:$HOME/.m2/repository/org/json/json/20180813/json-20180813.jar
 java -cp $CLASSPATH -jar $HOME/.m2/repository/org/xtuml/CarParkTestbench/1.0.0-SNAPSHOT/CarParkTestbench-1.0.0-SNAPSHOT.jar
